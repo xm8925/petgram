217944fdd4a92d39e694d536426053ca37d82bf2
xm8925 <innocence510@gmail.com>
2022-07-06 16:20:35 -0700

    style

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,12 @@
+console.log("Page is loading");
+
+
+
+function toggle() { 
+  var x = document.getElementById("myForm"); 
+  if (x.style.display === "block") { 
+    x.style.display = "none"; 
+  } else { 
+    x.style.display = "block"; 
+  } 
+} 
